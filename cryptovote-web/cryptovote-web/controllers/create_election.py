e0de87a8b0979825441d221366bce7e20e55945e
Nicholas Boucher <nboucher@college.harvard.edu>
2019-05-02 22:10:55 -0400

    Add email/phone/name confirmation pages to Web UI

--- cryptovote-web/cryptovote-web/controllers/create_election.py
+++ cryptovote-web/cryptovote-web/controllers/create_election.py
@@ -5,4 +5,14 @@ blueprint = Blueprint('create_election', __name__)
 
 @blueprint.route('/create')
 def create_name():
-    return render_template('create_election/create_name.html')
+    return render_template('create_election/election_name.html')
+
+
+@blueprint.route('/verify-email')
+def verify_email():
+
+
+    return render_template('create_election/verify_email.html')
+@blueprint.route('/verify-phone')
+def verify_phone():
+    return render_template('create_election/verify_phone.html')
