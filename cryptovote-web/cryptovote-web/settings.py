82de4e4c38b1872edbdb9909ba120ed4187f4e06
Nicholas Boucher <nboucher@college.harvard.edu>
2019-05-02 23:13:10 -0400

    Add more useful error message for missing .env file

--- cryptovote-web/cryptovote-web/settings.py
+++ cryptovote-web/cryptovote-web/settings.py
@@ -2,8 +2,16 @@
 
 import os
 from dotenv import load_dotenv
+
+# Load the .env file into the environment
 load_dotenv()
 
+# Ensure that a .env file was loaded
+if os.getenv('DATABASE_URL') is None:
+    raise NameError("Some environment variables not defined. "
+                    "Have you created a .env file?")
+
+# Load variables from environment
 ENV = os.getenv('FLASK_ENV', default='production')
 DEBUG = ENV == 'development'
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
