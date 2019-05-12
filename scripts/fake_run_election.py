6d7c7963ad4d5cdb01d9dfea4fe6a40e2df1e3b3
Kyle Swanson <swansonk.14@gmail.com>
2019-05-12 14:49:48 -0400

    Parsing options for run_election.py

--- scripts/fake_run_election.py
+++ scripts/fake_run_election.py
@@ -120,12 +120,27 @@ def fake_tally(ballots: List[CandidateOrderBallot],
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--master_lookup', type=str, required=True,
-                        help='Path to a .txt file containing a master lookup')
+                        help='Path to a .txt file containing a master lookup.')
     parser.add_argument('--ballot_image', type=str, required=True,
-                        help='Path to a .txt file containing a ballot image')
+                        help='Path to a .txt file containing a ballot image.')
+    parser.add_argument('--seats', type=int, default=1,
+                        help='The number of candidates that need to be elected.')
+    parser.add_argument('--n_bits', type=int, default=64,
+                        help='The number of bits to use in the public and private keys.')
+    parser.add_argument('--s', type=int, default=1,
+                        help='The power to which n = p * q will be raised. Plaintexts live in Z_n^s.')
+    parser.add_argument('--threshold', type=int, default=3,
+                        help='The minimum number of PrivateKeyShares needed to decrypt an encrypted number.')
+    parser.add_argument('--n_shares', type=int, default=3,
+                        help='The number of PrivateKeyShares to generate.')
     args = parser.parse_args()
 
-    public_key, private_key_ring = keygen(n_bits=64, s=1, threshold=3, n_shares=3)
+    public_key, private_key_ring = keygen(
+        n_bits=args.n_bits,
+        s=args.s,
+        threshold=args.threshold,
+        n_shares=args.n_shares
+    )
 
     contest_id_to_contest = load_ballot_data(
         master_lookup_path=args.master_lookup,
@@ -143,7 +158,7 @@ if __name__ == '__main__':
         start = time.time()
         result = fake_tally(
             ballots=contest['ballots'],
-            seats=1,
+            seats=args.seats,
             stop_candidate=contest['stop_candidate_id'],
             private_key_ring=private_key_ring
         )
