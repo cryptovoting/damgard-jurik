797610a02afe2fe971de1a088260fa5b33da7a93
Nicholas Thompson <NCGThompson@gmail.com>
2021-04-29 17:26:38 -0400

    Remove gmpy2 from dependency list in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setuptools.setup(
     url='https://github.com/cryptovoting/damgard-jurik',
     packages=setuptools.find_packages(),
     install_requires=[
-        'gmpy2',
         'pycryptodome'
     ],
     classifiers=[
